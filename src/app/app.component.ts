4cf47316a55dce21dacaf707cf4e62e8754a8a6c
Alan <alannavaseg_02@hotmail.com>
2020-05-02 17:13:40 -0500

    Entendiendo Andular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'hola a esto';
+  rockBands = ['nirvana', 'banda 2', 'banda3', 'banda4'];
+  rockBandName = '';
+  changeTitle(){
+    this.title = 'Nuevo titulo';
+  }
+  addRockBand(){
+    this.rockBands.push(this.rockBandName);
+    this.rockBandName = '';
+  }
 }
